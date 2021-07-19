2542e578cef28de95d39d4be5b3dd111e5a70682
cpingor <chipingchuan@hotmail.com>
2021-07-19 09:19:54 +0800

    undown

---
+++ 1255.得分最高的单词集合.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=1255 lang=python3
+#
+# [1255] 得分最高的单词集合
+#
+
+# @lc code=start
+class Solution:
+    def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
+# @lc code=end
+
